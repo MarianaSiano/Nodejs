8373eba226bcac37dfea2f23f38768e7dde54f1a
Mariana <maryhsiano@hotmail.com>
2023-07-28 13:17:19 -0300

    Inicializando

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const app = express();
+const port = 3000; //Variavel de ambiente
+
+app.get('/', (req, res) => {
+    res.send('Olá Mundo!')
+});
+
+app.listen(port, () => {
+    console.log(`App rodando em http://localhost:${port}`)
+});
