ea80e0b53d975a414e1c732a1cd76e0b9682173d
Mariana <maryhsiano@hotmail.com>
2023-07-28 13:24:14 -0300

    Atualizando json

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000; //Variavel de ambiente
+const path = require('path');
+const basePath = __dirname;
 
 app.get('/', (req, res) => {
     res.send('Olá Mundo!')
